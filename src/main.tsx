f1421802f8e9571e144cd56387322594fbb77193
sanjaysahu22 <sanjaysahu6243@gmail.com>
2025-04-11 02:38:12 +0530

    main.tsx correct

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,25 @@
-// src/main.tsx
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { UserProvider } from './utils/usercontext';
 import './index.css';
 import { Toaster } from "sonner";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+
 // Create root element
 const rootElement = document.getElementById('root');
-
 if (!rootElement) {
   throw new Error('Root element not found');
 }
-
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <BrowserRouter>
-    <UserProvider>
-      <App />
-      <Toaster richColors position="top-right" /> 
-    </UserProvider>
- </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+        <Toaster richColors position="top-right" />
+      </UserProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
